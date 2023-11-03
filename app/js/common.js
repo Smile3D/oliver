244431bed2deebcbd6c73e4f788a5f8ef8b49ece
Aleksandrov Sergey <Aleks140988@gmail.com>
2023-11-03 16:25:06 +0200

    Update

--- app/js/common.js
+++ app/js/common.js
@@ -1,8 +1,6 @@
-import $ from 'jquery'; window.jQuery = $; window.$ = $
+import $ from "jquery";
+window.jQuery = $;
+window.$ = $;
 // require('./other_script.js') // Require Other Script(s) from app/js folder Example
 
-document.addEventListener('DOMContentLoaded', () => {
-
-//Custom JS
-
-})
+document.addEventListener("DOMContentLoaded", () => {});
