870ca03bb658ae5447726d78c47647af896f7b56
Aleksandrov Sergey <Aleks140988@gmail.com>
2023-11-04 22:30:44 +0200

    Update

--- app/js/common.js
+++ app/js/common.js
@@ -3,4 +3,17 @@ window.jQuery = $;
 window.$ = $;
 // require('./other_script.js') // Require Other Script(s) from app/js folder Example
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+	if (window.innerWidth < 1024) {
+		$(".choose-lang").on("click", function () {
+			$(".lang").toggleClass("open-drop-down-menu");
+
+			$(document).on("click", function (event) {
+				if (!$(event.target).closest(".lang").length) {
+					// Закройте выпадающее меню, если клик был вне блока .lang
+					$(".lang").removeClass("open-drop-down-menu");
+				}
+			});
+		});
+	}
+});
